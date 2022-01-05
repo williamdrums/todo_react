64f9b656bf7cf24f7771e417df7c2c1634f6bfc9
MacWilliam <macwilliam@MacBook-Pro-de-MacWilliam.local>
2022-01-05 16:46:37 -0400

    Criando app TODO LIST com React

---
+++ src/component/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+  <div>
+      <h1>Meu primeiro App React</h1>
+      <Navbar></Navbar>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () =>{
+ return <div>Menu Principal</div>
+}
+
+export default Navbar
